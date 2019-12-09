8dc3aca4b9763b924186e49f2e7efe3f945fbb9b
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-12-09 20:46:48 +0700

    up

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -31,7 +31,7 @@ const Template = ({ data }) => {
 
       <Navbar />
 
-      <Helmet title={`5 giây Thông Minh | ${post.frontmatter.title}`} />
+      <Helmet title={`Televba Mỗi Ngày | ${post.frontmatter.title}`} />
 
       <div className="blog-post-header" style={{ backgroundImage: `url(${ post.frontmatter.image })` }}>
 
