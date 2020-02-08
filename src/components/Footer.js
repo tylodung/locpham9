f69c0b08ec46ac0943b0c05574f95c941edb7617
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2020-02-08 16:32:04 +0700

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () =>
     <footer className="site-footer outer">
         <div className="site-footer-content inner">
             <section className="copyright">
-                <Link to="/">Televba.com</Link> | Mẹo Hay Mỗi Ngày
+                <Link to="/">Televba.com</Link> | Mẹo Hay Của Tôi
             </section>
             <nav>
                 <a className="social-logo" href="https://youtu.be/N4WTi9EHLqA" >
