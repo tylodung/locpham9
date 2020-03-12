7151cd6a3c506c99e99a7affd845e4b25274e019
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2020-03-12 07:15:01 +0700

    up

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -31,7 +31,7 @@ const Template = ({ data }) => {
 
       <Navbar />
 
-      <Helmet title={`Nhật Ký Mẹo Vặt | ${post.frontmatter.title}`} />
+      <Helmet title={`Sổ Tay Mẹo Vặt | ${post.frontmatter.title}`} />
 
       <div className="blog-post-header" style={{ backgroundImage: `url(${ post.frontmatter.image })` }}>
 
