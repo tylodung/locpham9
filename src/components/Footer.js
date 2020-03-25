91c099212dc38accfe15a0126dde1293d2323398
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2020-03-25 19:08:28 +0700

    up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () =>
     <footer className="site-footer outer">
         <div className="site-footer-content inner">
             <section className="copyright">
-                <Link to="/">Sổ Tay Mẹo Vặt</Link> Copyright © 2020
+                <Link to="/">Lưu Bút</Link> Copyright © 2020
             </section>
             <nav>
                 <a className="social-logo" href="https://youtu.be/N4WTi9EHLqA" >
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -31,7 +31,7 @@ const Template = ({ data }) => {
 
       <Navbar />
 
-      <Helmet title={`Sổ Tay Mẹo Vặt | ${post.frontmatter.title}`} />
+      <Helmet title={`Lưu Bút | ${post.frontmatter.title}`} />
 
       <div className="blog-post-header" style={{ backgroundImage: `url(${ post.frontmatter.image })` }}>
 
